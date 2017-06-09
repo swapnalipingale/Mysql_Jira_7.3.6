47cc1d7927d8286c2bbda71346382da005fd94f7
swapnalipingale <swapnali.pingale@ecs.co.uk>
2017-06-09 11:32:24 +0530

    Update 99_mysql_setup.sh

--- my_init.d/99_mysql_setup.sh
+++ my_init.d/99_mysql_setup.sh
@@ -7,7 +7,7 @@ if [ ! -d /var/lib/mysql/mysql ]; then
     chown -R mysql.mysql /var/lib/mysql
     mysql_install_db > /dev/null
 
-    rm -rf /var/run/mysqld/*
+    #rm -rf /var/run/mysqld/*
 
     echo '*** Starting mysqld'
     # The sleep 1 is there to make sure that inotifywait starts up before the socket is created
